7d494d24dbeb1ad72b49fe6b2e579d317c06f5f4
thermo77 <68984191+thermo77@users.noreply.github.com>
2022-08-30 23:27:08 -0400

    Add files via upload

--- config.h
+++ config.h
@@ -7,15 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#1d1f21";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#eeeeee";
+static const char col_gray3[]       = "#eeeeee";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#360077";
+static const char col_cyan[]        = "#003153";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray1 },
 };
 
 /* tagging */
